d19492873e7945e00e379caf1582184bfced694f
lukin0110 <maarten.huijsmans@gmail.com>
2013-02-26 23:59:15 +0100

    remove gae

--- jara/pages.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-
-class IndexPage(webapp.RequestHandler):
-    def get(self):
-        values = {}
-        self.response.out.write(template.render('templates/index.html', values))
-
